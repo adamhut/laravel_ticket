82a2cd6b0d2445eb75650dcd8044b9e99efc79c8
adamhut <adamhut@gmail.com>
2017-09-11 07:29:20 -0700

    Promoter can visit their own concert list

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,11 @@ Route::post('/login','Auth\LoginController@login')->name('auth.login');
 Route::post('/logout','Auth\LoginController@logout')->name('auth.logout');
 
 Route::group(['middleware'=>'auth','prefix'=>'backstage','namespace'=>'Backstage'],function(){
+	Route::get('/concerts', 'ConcertsController@index')->name('backstage.concerts.index');
+	
 	Route::get('/concerts/new', 'ConcertsController@create')->name('backstage.show');
 	Route::post('/concerts', 'ConcertsController@store')->name('backstage.store');
 	
-	Route::get('/concerts', 'ConcertsController@index')->name('backstage.concerts.index');
 
 });
 
